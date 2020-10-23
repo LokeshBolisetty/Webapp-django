11f95ce32c8a66bab7152a991a143ac7e3527bdb
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-10-23 16:29:31 +0530

    Added documentation for appointment app

--- appointment/api.py
+++ appointment/api.py
@@ -6,14 +6,18 @@ from rest_framework import status
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+#This is the class which extends APIView from django rest framework. Usage of this class need authentication i.e the user has to be signed it to use this class.
+# This allows user to book a one on one appointment with mentor.
 class Appointmentlist(APIView):
     authentication_classes = [SessionAuthentication,TokenAuthentication]
     permission_classes = [IsAuthenticated]
+    #This is the overriden method get from APIView class. It displays all the appointments that are scheduled.
     def get(self,request):
         model = Appointment.objects.all()
         serializer = Appointmentserilizer(model,many=True)
         return Response(serializer.data)
     
+    #This method allows you to make a new appointment.
     def post(self,request):
     
         serializer = Appointmentserilizer(data=request.data)
@@ -23,6 +27,7 @@ class Appointmentlist(APIView):
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 class Appointmentid(APIView):
+    #This method displays only those appointments which match with the given id. If there is no such appointment it gives 404
     def get(self,request,id):
         model = Appointment.objects.filter(id__exact=id)
         if model:
@@ -30,7 +35,7 @@ class Appointmentid(APIView):
             return Response(serializer.data)
         else:
             return Response(f"Appointment doesn't exist with id {id}",status=status.HTTP_404_NOT_FOUND)
-    
+    #This method allows user to modify the appoitntment. Any field that is not given by the user will be set to the previous values.
     def put(self,request,id):
         try:
             model = Appointment.objects.get(id=id)
@@ -38,8 +43,8 @@ class Appointmentid(APIView):
             return Response(f"Appointment doesn't exist with id {id}",status=status.HTTP_404_NOT_FOUND)
         if model:
             serializer = Appointmentserilizer(model,data=request.data)
-            print(model.id)
-            print(model)
+            # print(model.id)
+            # print(model)
             #if not request.data["name"]:
             #    request.data["name"] = model.name
             if request.data.get('user',0) == 0:
@@ -61,7 +66,7 @@ class Appointmentid(APIView):
                 
                 return Response(serializer.data,status=status.HTTP_201_CREATED)
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-        
+    #This method allows user to delete an existing appointment by its id   
     def delete(self,request,id):
         try:
             model = Appointment.objects.get(id=id)
@@ -72,6 +77,7 @@ class Appointmentid(APIView):
         return Response(status=status.HTTP_200_OK)
 
 class Appointmentuserid(APIView):
+    #This method prints the appointments of a particular customer by his ID.
     def get(self,request,user_id):
         model = Appointment.objects.filter(user__exact=user_id)
         if model:
@@ -81,6 +87,7 @@ class Appointmentuserid(APIView):
             return Response(f"User doesn't exist with id {user_id}",status=status.HTTP_404_NOT_FOUND)
 
 class Appointmentmentorid(APIView):
+    #This method prints the appointments of a particular mentor by his name.
     def get(self,request,mentor_id):
         model = Appointment.objects.filter(appointment_with__exact=mentor_id)
         if model:
--- appointment/serializers.py
+++ appointment/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from  .models import Appointment
 
+#Serializer parses SQL data to JSON to facilitate usage in web apps.
 class Appointmentserilizer(serializers.ModelSerializer):
-    
+    #__all__ in fields parse all the fields in the Appointment model imported from models.py in this app
     class Meta:
         model = Appointment
         fields = '__all__'
